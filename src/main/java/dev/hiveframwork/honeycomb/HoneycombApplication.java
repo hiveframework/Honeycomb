dccdd6468f23a2f3d7479c86ebf42b6de79b034e
Anita Anderson <anita@ansan.io>
2023-07-23 23:33:30 -0400

    Start work on Honeycomb
    Signed-off-by: Anita Anderson <anita@ansan.io>

---
+++ src/main/java/dev/hiveframwork/honeycomb/HoneycombApplication.java
@@ -0,0 +1,13 @@
+package dev.hiveframwork.honeycomb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneycombApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneycombApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hiveframwork/honeycomb/Honeycomb/HoneycombApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hiveframwork.honeycomb.Honeycomb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneycombApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
